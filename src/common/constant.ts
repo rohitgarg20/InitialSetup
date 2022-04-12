fdc0e1105f07d5772a68833db525b471bcd1d24d
shyam <shyam.pandit@edfora.com>
2022-04-12 12:01:58 +0530

    UserReview Slider Design update

--- src/common/constant.ts
+++ src/common/constant.ts
@@ -25,7 +25,7 @@ export const MAIN_STACK_KEYS = {
   TAB_BAR_STACK: 'tabBarStack'
 }
 
-export const BASE_URL = 'http://192.168.1.203:4567'
+export const BASE_URL = 'http://192.168.99.164:4567'
 // export const BASE_URL = 'http://sdlms.deepthought.education'
 
 export const DAYS_NAME = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat']
