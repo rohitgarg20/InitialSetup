02ef457513dd2ac0050131c55a33a9e55bfdd032
Rohit Garg <rohitgarg@EDILT5105s-MacBook-Pro.local>
2022-05-14 14:57:41 +0530

    package name update to socio wood

--- ios/deepthoughtTests/deepthoughtTests.m
+++ ios/deepthoughtTests/deepthoughtTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface deepthoughtTests : XCTestCase
+@interface socialwoodTests : XCTestCase
 
 @end
 
-@implementation deepthoughtTests
+@implementation socialwoodTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
